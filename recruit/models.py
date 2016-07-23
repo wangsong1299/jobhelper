52ff3d487108da7a49887eaf767f6c968b3d453f
wangsong <wangsong1299@163.com>
2016-07-23 17:59:20 +0800

    去掉公司logo

--- recruit/models.py
+++ recruit/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 # Create your models here.
 class Recruit(models.Model):
-	DEFAULT_LOGO='http://wx.qlogo.cn/mmopen/LIND77SSexibODU6MXJnyy4b4mxObW6IfFFgPydwOyYnI8XYiaSA66xXX6Z1a0mxVezV722ibqHONdJHwY7RBeAmw/0'
 	STATE_CHOICES = (
 		(0, '正常'),
 		(1, '已过期'),
@@ -22,8 +21,6 @@ class Recruit(models.Model):
 	address=models.CharField(max_length = 30,blank=True)
 	# 薪水
 	salary=models.CharField(max_length = 30,blank=True)
-	# 公司logo
-	logo = models.CharField(max_length = 200, blank = True, default = DEFAULT_LOGO)
 	# 职位描述
 	description=models.CharField(max_length = 30)
 	# 联系人
--- recruit/views.py
+++ recruit/views.py
@@ -13,7 +13,7 @@ def index(request):
 	recruit_info={}
 	i=0
 	for r in recruits:
-		recruit_info[i]={'position':r.position,'company':r.company,'degree':r.degree,'years':r.years,'sex':r.sex,'address':r.address,'salary':r.salary,'logo':r.logo,'description':r.description,'id':r.id}
+		recruit_info[i]={'position':r.position,'company':r.company,'degree':r.degree,'years':r.years,'sex':r.sex,'address':r.address,'salary':r.salary,'description':r.description,'id':r.id}
 		i=i+1
 	return render_to_response('recruit.html',{'recruit_info':recruit_info})
 #首页
@@ -26,7 +26,7 @@ def myshow(request,id):
 	for c in connects:
 		recruit_id=c.recruit_id
 		r=Recruit.objects.filter(id=recruit_id)[0]
-		myshow_info[i]={'position':r.position,'company':r.company,'years':r.years,'degree':r.degree,'sex':r.sex,'salary':r.salary,'logo':r.logo,'id':recruit_id,'state':c.state}
+		myshow_info[i]={'position':r.position,'company':r.company,'years':r.years,'degree':r.degree,'sex':r.sex,'salary':r.salary,'id':recruit_id,'state':c.state}
 		i=i+1
 	return render_to_response('myrecruit.html',{'myshow_info':myshow_info})
 
